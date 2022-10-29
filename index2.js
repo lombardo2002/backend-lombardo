e4c52bb553b5927078bd3343b148b13d29a924e6
lombardo2002 <107504030+lombardo2002@users.noreply.github.com>
2022-10-29 16:48:25 -0300

    Delete index2.js

--- index2.js
+++
@@ -1,9 +0,0 @@
-import http from 'http'; 
-
-const server = http.createServer((peticion,respuesta)=>{
-    respuesta.end('Hola')
-})
-
-const connectedServer = server.listen(8080,()=>{
-    console.log("primer servidor escuchando")
-})
